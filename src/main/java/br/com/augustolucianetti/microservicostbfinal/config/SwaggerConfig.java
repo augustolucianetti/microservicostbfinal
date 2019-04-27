4c3bfbbabeae4564a7ad2b84642ff0bb4dd7efd6
Augusto César Favaretto Lucianetti <augusto_lucianetti@hotmail.de>
2019-04-27 08:18:16 -0300

    adicionando swagger

---
+++ src/main/java/br/com/augustolucianetti/microservicostbfinal/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package br.com.augustolucianetti.microservicostbfinal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket greetingApi() {
+        return new Docket( DocumentationType.SWAGGER_2 )
+                .select()
+                .apis( RequestHandlerSelectors.basePackage( "br.com.augustolucianetti.microservicostbfinal" ) )
+                .build()
+                .apiInfo( metaData() );
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Trabalho final REST API")
+                .description("\"TRabalho final  REST API for FIAP\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
